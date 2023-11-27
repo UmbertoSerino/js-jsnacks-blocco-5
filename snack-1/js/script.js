dbc1392274096fa8464fbb918b73724805476cf6
UmbertoSerino <serumberto@gmail.com>
2023-11-27 11:40:43 +0100

    created folder and array in js

---
+++ snack-1/js/script.js
@@ -0,0 +1,20 @@
+// Crea un array composto da 10 automobili.
+// Ogni oggetto automobile avrà le seguenti proprietà: marca, model e alimentazione (benzina, diesel, gpl, elettrico, metano).
+
+// Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+ 
+// Infine stampa separatamente i 3 array.
+
+const carsList = [
+    { brad: 'Toyota', model: 'Yaris', fuel: 'benzina' },
+    { brad: 'Volkswagen', model: 'Golf', fuel: 'diesel' },
+    { brad: 'Ferrari', model: 'V10', fuel: 'elettrico' },
+    { brad: 'Fiat', model: 'Panda', fuel: 'gpl' },
+    { brad: 'Renault', model: 'Clio', fuel: 'diesel' },
+    { brad: 'AstonMartin', model: 'Valhalla', fuel: 'elettrico' },
+    { brad: 'Ford', model: 'Focus', fuel: 'benzina' },
+    { brad: 'Cytroen', model: 'C3', fuel: 'diesel' },
+    { brad: 'Hyundai', model: 'Kona', fuel: 'elettrico' },
+    { brad: 'Mercedes', model: 'GLC', fuel: 'metano' },
+];
+
