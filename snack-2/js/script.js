827a424e8447093fcffef88f6e98a36a4ad50276
UmbertoSerino <serumberto@gmail.com>
2023-11-27 15:44:59 +0100

    morning snack complete

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -4,10 +4,22 @@
 // Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
 
 const usersList = [
-    {firstName: `Carlotta`, lastName: `Della Cornovaglia`, age: `18` };
-    {firstName: `Carla`, lastName: `Cracca`, age: `67` };
-    {firstName: `Gina`, lastName: `Stefan`, age: `20` };
-    {firstName: `Jacque`, lastName: `Yesman`, age: `30` };
-    {firstName: `Primo`, lastName: `Seguace`, age: `12` };
-    {firstName: `Gino`, lastName: `Della Monaca`, age: `10` };
+    {firstName: `Carlotta`, lastName: `Della Cornovaglia`, age: 18 },
+    {firstName: `Carla`, lastName: `Cracca`, age: 67 },
+    {firstName: `Gina`, lastName: `Stefan`, age: 20 },
+    {firstName: `Jacque`, lastName: `Yesman`, age: 30 },
+    {firstName: `Primo`, lastName: `Seguace`, age: 12 },
+    {firstName: `Gino`, lastName: `Della Monaca`, age: 10 },
 ];
+
+console.log(usersList);
+
+const ageToDrive = usersList.map((singlePerson) => {
+    singlePerson.nomeCompleto = singlePerson.firstName + "" + singlePerson.lastName;
+    singlePerson.maggiorenne = singlePerson.age >= 18;
+    return singlePerson;
+});
+
+const adults = ageToDrive.filter ((person) => person.maggiorenne === true);
+console.log(adults);
+
